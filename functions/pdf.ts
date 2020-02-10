9cc61c587b76ede57a23d91673c2f46ba865dafb
Crespo Wang <crespo.wang@inquisitive.com>
2020-02-10 17:20:00 +1100

    step 1 demo

---
+++ functions/pdf.ts
@@ -0,0 +1,30 @@
+import middy from "@middy/core";
+import doNotWaitForEmptyEventLoop from "@middy/do-not-wait-for-empty-event-loop";
+import PDFDocument from "pdfkit";
+
+const generatePdf = async (): Promise<Buffer> => {
+  return new Promise((resolve: Function, reject: Function): any => {
+    const doc = new PDFDocument();
+    doc.text("How Are You");
+    doc.end();
+    const buffers: any = [];
+    doc.on("data", buffers.push.bind(buffers));
+    doc.on("end", () => {
+      const pdfData = Buffer.concat(buffers);
+      resolve(pdfData);
+    });
+  });
+};
+const handler = async (event: any) => {
+  const stream = await generatePdf();
+  return {
+    statusCode: 200,
+    isBase64Encoded: true,
+    headers: {
+      "Content-type": "application/pdf"
+    },
+    body: stream.toString("base64")
+  };
+};
+
+export const generate = middy(handler).use(doNotWaitForEmptyEventLoop());
